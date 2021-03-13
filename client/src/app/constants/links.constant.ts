77f3674c7da347380807576c4c73ec4f218b4851
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-03-13 15:38:21 +0100

    fix: netlify proxy not work, so use request link complete

--- client/src/app/constants/links.constant.ts
+++ client/src/app/constants/links.constant.ts
@@ -1,21 +1,22 @@
-// const serverBaseUrl = import.meta.env.VITE_SERVER_BASE_URL;
+const serverBaseUrl =
+  import.meta.env.VITE_SERVER_BASE_URL || 'https://td-server-bookstore.herokuapp.com';
 export const SERVER_LINKS = {
-  authRefresh: '/api/auth/refresh', // POST
-  authAutoRefresh: '/api/auth/auto-refresh',
-  authLogin: '/api/auth/login', // POST with body {userOrEmail, password}
-  authRegister: '/api/auth/register', // POST with body {username, email, password}
-  authActivate: '/api/auth/activate', //GET with query ?token=
-  authMe: '/api/auth', // GET
-  authForgotPassword: '/api/auth/forgot-password', //POST with body {email}
-  authResetPassword: '/api/auth/reset-password', // GET with body {token, newPassword}
-  authLogout: '/api/auth', // DELETE
-  userAddToCart: '/api/users/purchase', // POST
-  userUpdateCart: '/api/users/update-cart-item', // POST
-  userRemoveFromCart: '/api/users/remove-cart-item', // POST
-  userRemoveAllFromCart: '/api/users/remove-carts', // POST
-  userUpload: '/api/users/upload', // POST with param {file: FileType}
-  bookGetById: '/api/books/book', //GET with param /:id
-  bookGetByGenre: '/api/books/genre', // GET with param /:genre
-  bookSearch: '/api/books/search', // GET with query ?q=
-  bookQuery: '/api/books/query', // GET with query ?q=
+  authRefresh: serverBaseUrl + '/api/auth/refresh', // POST
+  authAutoRefresh: serverBaseUrl + '/api/auth/auto-refresh',
+  authLogin: serverBaseUrl + '/api/auth/login', // POST with body {userOrEmail, password}
+  authRegister: serverBaseUrl + '/api/auth/register', // POST with body {username, email, password}
+  authActivate: serverBaseUrl + '/api/auth/activate', //GET with query ?token=
+  authMe: serverBaseUrl + '/api/auth', // GET
+  authForgotPassword: serverBaseUrl + '/api/auth/forgot-password', //POST with body {email}
+  authResetPassword: serverBaseUrl + '/api/auth/reset-password', // GET with body {token, newPassword}
+  authLogout: serverBaseUrl + '/api/auth', // DELETE
+  userAddToCart: serverBaseUrl + '/api/users/purchase', // POST
+  userUpdateCart: serverBaseUrl + '/api/users/update-cart-item', // POST
+  userRemoveFromCart: serverBaseUrl + '/api/users/remove-cart-item', // POST
+  userRemoveAllFromCart: serverBaseUrl + '/api/users/remove-carts', // POST
+  userUpload: serverBaseUrl + '/api/users/upload', // POST with param {file: FileType}
+  bookGetById: serverBaseUrl + '/api/books/book', //GET with param /:id
+  bookGetByGenre: serverBaseUrl + '/api/books/genre', // GET with param /:genre
+  bookSearch: serverBaseUrl + '/api/books/search', // GET with query ?q=
+  bookQuery: serverBaseUrl + '/api/books/query', // GET with query ?q=
 };
